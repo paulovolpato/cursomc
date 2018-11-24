1600e515a7826c621e0be23f1b05da8c4e83518a
Paulo Volpato <paulovolpato.ti@gmail.com>
2018-11-24 16:45:02 -0200

    Projeto criado

---
+++ src/main/java/br/com/paulo/aulajpa/AulaJpaApplication.java
@@ -0,0 +1,12 @@
+package br.com.paulo.aulajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaJpaApplication.class, args);
+	}
+}
