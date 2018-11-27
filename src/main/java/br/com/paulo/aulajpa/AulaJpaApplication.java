b7d0efcc027a22c11513d5d5e70f40df790ee998
Paulo Volpato <paulovolpato.ti@gmail.com>
2018-11-27 11:31:53 -0200

    Criando operacao de instanciacao

--- src/main/java/br/com/paulo/aulajpa/AulaJpaApplication.java
+++ src/main/java/br/com/paulo/aulajpa/AulaJpaApplication.java
@@ -1,12 +1,30 @@
 package br.com.paulo.aulajpa;
 
+import br.com.paulo.aulajpa.domain.Categoria;
+import br.com.paulo.aulajpa.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AulaJpaApplication {
+import java.util.Arrays;
 
-	public static void main(String[] args) {
-		SpringApplication.run(AulaJpaApplication.class, args);
-	}
+@SpringBootApplication
+public class AulaJpaApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AulaJpaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria cat1 = new Categoria(null, "Informatica");
+        Categoria cat2 = new Categoria(null, "Escritorio");
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+    }
 }
