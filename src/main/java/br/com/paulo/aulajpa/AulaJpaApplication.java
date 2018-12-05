e6c697b393421c5286a2cf7ae0bec3eb32d27954
Paulo Volpato <paulovolpato.ti@gmail.com>
2018-12-05 14:38:24 -0200

    Produto e associacao muitos para muitos

--- src/main/java/br/com/paulo/aulajpa/AulaJpaApplication.java
+++ src/main/java/br/com/paulo/aulajpa/AulaJpaApplication.java
@@ -1,7 +1,9 @@
 package br.com.paulo.aulajpa;
 
 import br.com.paulo.aulajpa.domain.Categoria;
+import br.com.paulo.aulajpa.domain.Produto;
 import br.com.paulo.aulajpa.repositories.CategoriaRepository;
+import br.com.paulo.aulajpa.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,6 +17,9 @@ public class AulaJpaApplication implements CommandLineRunner {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
     public static void main(String[] args) {
 
         SpringApplication.run(AulaJpaApplication.class, args);
@@ -25,6 +30,18 @@ public class AulaJpaApplication implements CommandLineRunner {
         Categoria cat1 = new Categoria(null, "Informatica");
         Categoria cat2 = new Categoria(null, "Escritorio");
 
+        Produto p1 = new Produto(null, "Computador", 2000.00);
+        Produto p2 = new Produto(null, "Impressora", 800.00);
+        Produto p3 = new Produto(null, "Mouse", 80.00);
+
+        cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+        cat2.getProdutos().addAll(Arrays.asList(p2));
+
+        p1.getCategorias().addAll(Arrays.asList(cat1));
+        p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+        p3.getCategorias().addAll(Arrays.asList(cat1));
+
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
     }
 }
---
+++ src/main/java/br/com/paulo/aulajpa/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package br.com.paulo.aulajpa.repositories;
+
+import br.com.paulo.aulajpa.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+
+}
